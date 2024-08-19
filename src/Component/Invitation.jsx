371f8ea3b74a73f949e4206190e6912983551d08
Alamin-22 <mdalaminmollick@gmail.com>
2024-08-20 05:33:57 +0600

    finally

--- src/Component/Invitation.jsx
+++ src/Component/Invitation.jsx
@@ -8,14 +8,14 @@ import GreenEmoji from "@/Images/green character 1.png"
 const Invitation = () => {
     return (
         <>
-            <div className=" relative h-[15rem] md:h-[28rem] xl:h-[35rem] border">
+            <div className=" relative h-[15rem] md:h-[28rem] xl:h-[35rem] ">
 
                 <figure className=" ">
                     <Image src={BlueBgLeft} alt="Banner Effect" className="h-[15rem] md:h-[28rem] xl:h-[35rem] absolute top-0 left-0 " />
                     <Image src={LeftBigCoin} alt="Banner Effect" className="w-[25%] absolute top-16 md:top-36 left-0 " />
-                    <Image src={CenterCoin} alt="Banner Effect" className=" w-[60%] md:w-[70%] xl:w-full absolute top-14 md:top-24 xl:top-0 left-9  md:left-12 xl:left-24 " />
-                    <Image src={InviteText} alt="Banner Effect" className=" w-[25%] absolute top-24 right-24 md:right-48 xl:right-72  " />
-                    <Image src={GreenEmoji} alt="Banner Effect" className=" max-w-7xl mx-auto w-20 md:w-40 xl:w-72 absolute top-16 xl:top-32 right-0  " />
+                    <Image src={CenterCoin} alt="Banner Effect" className=" w-[60%] md:w-[70%]  absolute top-14 md:top-24 lg:top-8  xl:top-0 left-9  md:left-12 xl:left-24 " />
+                    <Image src={InviteText} alt="Banner Effect" className=" w-[35%] md:w-[25%] absolute top-24 md:top-40 right-12 md:right-48 xl:right-72  " />
+                    <Image src={GreenEmoji} alt="Banner Effect" className=" max-w-7xl mx-auto w-20 md:w-40 xl:w-72 absolute top-20 xl:top-28 right-0  " />
                 </figure>
             </div>
         </>
